beb611bf18cbe645f5425a3391ff3a8317dba4c8
MrTupko <igst200@outlook.com>
2024-12-08 16:17:54 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 function expand(elem, id) {
     let answer =  document.querySelector(`#${id}`);
     if (!answer.classList.contains("info-grow")) {
-        elem.querySelector("img").src = "../assets/images/icon-minus.svg";
+        elem.querySelector("img").src = "/assets/images/icon-minus.svg";
         answer.classList.add("info-grow");
     } else {
         answer.classList.remove("info-grow");
-        elem.querySelector("img").src = "../assets/images/icon-plus.svg";
+        elem.querySelector("img").src = "/assets/images/icon-plus.svg";
     }
 }
